3d03aa5191d7c149a4603e21664773a7e54ac21a
Roman Ivakhiv <megazorch@gmail.com>
2023-03-31 21:44:55 +0300

    Added simple views to render shop.html

--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from .models import Broker, Yacht, Specifications, Propulsion
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.views import generic
+from django.db.models import F
 
 def index(request):
     """View function for home page of site."""
 
     # Generate counts of some of the main objects
     num_yachts = Yacht.objects.all().count()
+    yachts = Yacht.objects.order_by('-price')[:3]
+
+
 
     context = {
         'num_yachts': num_yachts,
+        'yachts': yachts,
         'num_instances': '1',
         'num_instances_available': '1',
         'num_authors': '1',
@@ -21,3 +24,11 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+def shop(request):
+    """View function for shop page of site."""
+    context = {
+
+    }
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'shop.html', context=context)
