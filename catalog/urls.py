c05dc4b133f6ddc23a717792c8c048e55e5fb731
Roman Ivakhiv <megazorch@gmail.com>
2023-04-02 07:19:37 +0300

    Added path for yacht-detail view.

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('shop', views.shop, name='shop'),
+    path('shop/', views.YachtListView.as_view(), name='shop'),
+    path('yacht/<int:pk>', views.YachtDetailView.as_view(), name='yacht-detail')
 ]
