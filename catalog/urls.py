cc8308d3c37bcc67eb9812d265ddb0da1534ceba
Roman Ivakhiv <megazorch@gmail.com>
2023-04-08 08:30:03 +0300

    Added path for shop page with full list of yachts and page with full details of the yacht.

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('shop/', views.YachtListView.as_view(), name='shop'),
-    path('yacht/<int:pk>', views.YachtDetailView.as_view(), name='yacht-detail')
+    path('yachts/', views.YachtListView.as_view(), name='shop'),
+    path('yachts/<int:pk>', views.YachtDetailView.as_view(), name='yacht-detail')
 ]
