3b8d8c1f7c0b3f9c4a1fd5f3cc45c2a654d3010e
Roman Ivakhiv <megazorch@gmail.com>
2023-04-17 22:06:11 +0300

    Passed Category objects to YachtListView for shop.html

--- catalog/views.py
+++ catalog/views.py
@@ -35,7 +35,10 @@ class YachtListView(generic.ListView):
             queryset = queryset.filter(category__id=category_id)
         return queryset
 
-
+    def get_context_data(self, **kwargs):       # ChatGPT
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
 
 class YachtDetailView(generic.DetailView):
     model = Yacht
