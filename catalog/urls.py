d6b17ef3e252c74be0d997d6a234b70628e1d9d3
Roman Ivakhiv <megazorch@gmail.com>
2023-04-22 08:44:03 +0300

    Added path for cart.html

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('yachts/', views.YachtListView.as_view(), name='shop'),
     path('yachts/<int:pk>', views.YachtDetailView.as_view(), name='yacht-detail'),
     path('__debug__/', include(debug_toolbar.urls)),
+    path('cart/<int:pk>', views.CartDetailView.as_view(), name='cart'),
 ]
