6d8fc5fae5f1654467c81e2cc7167d18b34946d4
Roman Ivakhiv <megazorch@gmail.com>
2023-04-26 19:35:30 +0300

    Added path for build-in django authentication system.

--- yacht_shop/urls.py
+++ yacht_shop/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
