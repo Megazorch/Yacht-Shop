8dfa7b0ad98d16afea5f04532471dcbddeeeec60
Roman Ivakhiv <megazorch@gmail.com>
2023-05-03 20:00:47 +0300

    Refactored form by adding widget and attrs for it.

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,4 +1,11 @@
 from django import forms
 
+
 class AddToCartForm(forms.Form):
-    quantity = forms.IntegerField(min_value=1, initial=1)
+    quantity = forms.IntegerField(widget=forms.HiddenInput(
+        attrs={'class': 'list-inline-item text-right',
+               'name': 'product-quanity',
+               'id': 'product-quanity',
+               'value': '1',
+               }))
+    # quantity = forms.IntegerField(widget=QuantityInput)
