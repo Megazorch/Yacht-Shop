b891264bcc034720e5a08095281720f40a4967b8
Roman Ivakhiv <megazorch@gmail.com>
2023-05-21 10:17:45 +0300

    Added path for REST API framework.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from . import views
 import debug_toolbar
+from rest_framework.urlpatterns import format_suffix_patterns
+
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -8,4 +13,8 @@ urlpatterns = [
     path('yachts/<int:pk>', views.YachtDetailView.as_view(), name='yacht-detail'),
     path('__debug__/', include(debug_toolbar.urls)),
     path('yachts/cart/<int:pk>', views.CartDetailView.as_view(), name='cart'),
+    path('cart-line-items/', views.CartLineItemList.as_view()),
+    path('cart-line-items/<int:pk>/', views.CartLineItemDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
