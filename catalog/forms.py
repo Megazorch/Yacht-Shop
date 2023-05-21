b6ec4bce1a29ea4e08a29a48b7d7ca9ae0a12ab1
Roman Ivakhiv <megazorch@gmail.com>
2023-05-21 10:17:22 +0300

    Modified attributes of form for proper work of JavaScript.

--- catalog/forms.py
+++ catalog/forms.py
@@ -4,8 +4,8 @@ from django import forms
 class AddToCartForm(forms.Form):
     quantity = forms.IntegerField(widget=forms.HiddenInput(
         attrs={'class': 'list-inline-item text-right',
-               'name': 'product-quanity',
-               'id': 'product-quanity',
+               'name': 'product-quantity',
+               'id': 'product-quantity',
                'value': '1',
                }))
     # quantity = forms.IntegerField(widget=QuantityInput)
