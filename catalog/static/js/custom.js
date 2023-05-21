62a25476269dafe550771b9a927d5a85b4c2b730
Roman Ivakhiv <megazorch@gmail.com>
2023-05-21 10:45:40 +0300

    Deleted useless part of code.

--- catalog/static/js/custom.js
+++ catalog/static/js/custom.js
@@ -15,8 +15,7 @@ var currentCartLineItemId = parseInt(cartlineitemId.textContent);
 var newQuantity = currentQuantity + 1;
 
 function updateQuantity(currentCartLineItemId, currentQuantity) {
-  var username = 'user1';
-  var password = 'gaLAga123#';
+
   var url = '/catalog/cart-line-items/' + currentCartLineItemId + '/';
 
   var data = JSON.stringify({ quantity: currentQuantity });
@@ -25,11 +24,6 @@ function updateQuantity(currentCartLineItemId, currentQuantity) {
   var xhr = new XMLHttpRequest();
   xhr.open('PUT', url, true);
   xhr.setRequestHeader('Content-Type', 'application/json');
-
-  // Construct the Authorization header
-  var credentials = username + ':' + password;
-  var encodedCredentials = btoa(credentials);
-  xhr.setRequestHeader('Authorization', 'Basic ' + encodedCredentials);
 
   // Get the CSRF token value from the page's HTML
   var csrfToken = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
