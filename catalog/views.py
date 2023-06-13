48f98eb2aee9a35d8a1409034f49377f8af4d227
Roman Ivakhiv <megazorch@gmail.com>
2023-06-13 23:46:40 +0300

    Created view for Sing Up page.

--- catalog/views.py
+++ catalog/views.py
@@ -6,7 +6,8 @@ from .forms import *
 from catalog.models import CartLineItem
 from catalog.serializers import *
 from rest_framework import generics
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 from .permissions import IsOwnerOrReadOnly
 
@@ -117,6 +118,12 @@ class CartDetailView(generic.DetailView):
         context['cart_items'] = cart_items
         context['final_price'] = final_price
         return context
+
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/sign-up.html"
 
 
 # SERIALIZERS
