154cc958c9e7cb95a07515032e014a7577c77805
Roman Ivakhiv <megazorch@gmail.com>
2023-07-15 18:28:46 +0300

    Autopep8 and pylint changes

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,16 +10,44 @@ from . import views
 # Additionally, we include login URLs for the browsable API.
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('yachts/', views.YachtListView.as_view(), name='shop'),
-    path('yachts/<int:primary_key>', views.YachtDetailView.as_view(), name='yacht-detail'),
-    path('__debug__/', include(debug_toolbar.urls)),
-    path('yachts/cart/<int:pk>', views.CartDetailView.as_view(), name='cart'),
-    path('cart-line-items/', views.CartLineItemList.as_view()),
-    path('cart-line-items/<int:pk>/', views.CartLineItemDetail.as_view()),
-    path("accounts/sign-up/", views.SignUpView.as_view(), name="sign-up"),
-    path('about/', views.AboutView.as_view(), name='about'),
-    path('contact/', views.ContactView.as_view(), name='contact'),
+    path(
+        '',
+        views.index,
+        name='index'),
+    path(
+        'yachts/',
+        views.YachtListView.as_view(),
+        name='shop'),
+    path(
+        'yachts/<int:primary_key>',
+        views.YachtDetailView.as_view(),
+        name='yacht-detail'),
+    path(
+        '__debug__/',
+        include(
+            debug_toolbar.urls)),
+    path(
+        'yachts/cart/<int:pk>',
+        views.CartDetailView.as_view(),
+        name='cart'),
+    path(
+        'cart-line-items/',
+        views.CartLineItemList.as_view()),
+    path(
+        'cart-line-items/<int:pk>/',
+        views.CartLineItemDetail.as_view()),
+    path(
+        "accounts/sign-up/",
+        views.SignUpView.as_view(),
+        name="sign-up"),
+    path(
+        'about/',
+        views.AboutView.as_view(),
+        name='about'),
+    path(
+        'contact/',
+        views.ContactView.as_view(),
+        name='contact'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
